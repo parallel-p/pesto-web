d376a96e4c10c3de12460230921cdbc979a28140
Denis Kalinochkin <kalinochkind@gmail.com>
2015-12-29 18:56:30 +0300

    Fixed view

--- web/themes/themes_by_user.py
+++ web/themes/themes_by_user.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-from stats.models import User
+from stats.models import User, Participation
 from .models import UserResult
 
 def themes_by_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
-    parts = UserResult.objects.filter(user=user).values('participation').distinct().order_by('participation')
-    parts = [str(i['participation']) for i in parts]
+    parts = Participation.objects.filter(user=user).distinct().order_by('season')
     result = []
     for part in parts:
-        part_res = UserResult.objects.filter(user=user).filter(participation=part).order_by('solved')
+        part_res = UserResult.objects.filter(participation=part).order_by('solved')
         result.append([str(part_res[0].participation), []])
         for theme_res in part_res[::-1]:
             result[-1][1].append([theme_res.theme.name, theme_res.solved])
