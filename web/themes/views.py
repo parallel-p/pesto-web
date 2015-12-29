cd48b3c599f7647aa6419814869fccf787f3a495
Denis Kalinochkin <kalinochkind@gmail.com>
2015-12-29 16:16:27 +0300

    Fixed years ordering

--- web/themes/views.py
+++ web/themes/views.py
@@ -7,10 +7,10 @@ def by_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     years = UserResult.objects.filter(user=user).values('year').distinct()
     years = [str(i['year']) for i in years]
-    result = {}
+    result = []
     for year in years:
         year_res = UserResult.objects.filter(user=user).filter(year=year).order_by('solved')
-        result[year] = []
+        result.append([year, []])
         for theme_res in year_res[::-1]:
-            result[year].append([theme_res.theme.name, theme_res.solved])
+            result[-1][1].append([theme_res.theme.name, theme_res.solved])
     return render(request, 'by_user.html', {'result':result})
