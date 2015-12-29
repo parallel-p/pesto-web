957936381c626bc15dd4ef983d658355f8e637ec
Denis Kalinochkin <kalinochkind@gmail.com>
2015-12-29 09:26:33 +0300

    Master cleaned

--- test_coverage.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print('---')
-for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
-    for file in files:
-        if file.endswith('.py') and not file.endswith('_test.py'):
-            if file[:-3] + '_test.py' not in files:
-                print(os.path.join(root, file))
-print('---')
