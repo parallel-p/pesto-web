89ae93e0ed3c867b176e5b46eb9a35fa915e85cf
Denis Kalinochkin <kalinochkind@gmail.com>
2015-12-30 09:10:29 +0300

    Beautiful code

--- web/themes/themes_by_user.py
+++ web/themes/themes_by_user.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 from stats.models import User, Participation
 from .models import UserResult
 
+class PartResult:
+    def __init__(self, part):
+        self.part = part
+        self.themes = []
+
 def themes_by_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     parts = Participation.objects.filter(user=user).order_by('season')
     result = []
     for part in parts:
-        part_res = UserResult.objects.filter(participation=part).order_by('solved')
+        part_res = UserResult.objects.filter(participation=part).order_by('-solved')
         if not part_res:
             continue
-        result.append([str(part), []])
-        for theme_res in part_res[::-1]:
-            result[-1][1].append([theme_res.theme.name, theme_res.solved])
+        cur_res = PartResult(str(part))
+        for theme_res in part_res:
+            cur_res.themes.append([theme_res.theme.name, theme_res.solved])
+        result.append(cur_res)
     return result
