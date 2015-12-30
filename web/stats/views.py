b61f7375e4da7a70691bb6219ac33ea99b3e48f7
SpyCheese <mikle98@yandex.ru>
2015-12-30 12:52:14 +0300

    Добавлена форма для заполнения тем

--- web/stats/views.py
+++ web/stats/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import User
+from .models import *
 from themes.themes_by_user import themes_by_user
+from .forms import AdminThemesForm
+from django import forms
 
 def index(request):
     return HttpResponse("Sample text")
@@ -13,3 +15,44 @@ def profile(request, user_id):
 def users(request):
     users = User.objects.order_by('last_name', 'first_name')
     return render(request, 'users.html', {'users': users})
+
+def admin_themes(request):
+    seasons = []
+    for season in Season.objects.all():
+        parallels = []
+        for parallel in Parallel.objects.all():
+            contests = list(Contest.objects.filter(season=season, parallel=parallel))
+            if len(contests) == 0:
+                continue
+            parallels.append((parallel.name, contests))
+        if len(parallels) == 0:
+            continue
+        seasons.append((season.name, parallels))
+
+    top_str = ""
+    if request.method == 'POST':
+        form = AdminThemesForm(request.POST)
+        for field_name in form.data:
+            if len(field_name) < len('theme_') or field_name[:len('theme_')] != 'theme_':
+                continue
+            value = form.data[field_name]
+            if value == 'None':
+                value = None
+            elif value == 'Prev':
+                continue
+            else:
+                value = get_object_or_404(Theme, pk=int(value))
+            contest = get_object_or_404(Contest, pk=int(field_name[len('theme_'):]))
+            contest.theme = value
+            contest.save()
+        if form.is_valid():
+            top_str = 'Данные о темах обновлены'
+        else:
+            top_str = 'Что-то пошло не так'
+        return render(request, 'admin_themes.html', {'top_str': top_str})
+
+    form = AdminThemesForm()
+    return render(request, 'admin_themes.html', {'top_str': top_str,
+                                                 'seasons': seasons,
+                                                 'form': form,
+                                                 'themes': Theme.objects.all()})
