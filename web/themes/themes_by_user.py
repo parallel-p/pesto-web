3b64119b9c4534b715ba64a8cbc4f1fed056c5ac
SpyCheese <mikle98@yandex.ru>
2016-01-01 15:24:14 +0300

    Добавлена статистика тем по всем годам

--- web/themes/themes_by_user.py
+++ web/themes/themes_by_user.py
@@ -24,6 +24,7 @@ def themes_by_user(user_id):
     user = get_object_or_404(User, pk=user_id)
     parts = Participation.objects.filter(user=user).order_by('season')
     result = []
+    themes_total = dict()
     for part in parts:
         part_res = sorted(UserResult.objects.filter(participation=part), key=solved_percent, reverse=True)
         if not part_res:
@@ -31,8 +32,25 @@ def themes_by_user(user_id):
         cur_res = PartResult(str(part))
         for theme_res in part_res:
             cur_res.themes.append([theme_res.theme.name, theme_res.solved, theme_res.total, solved_percent(theme_res)])
+            theme = theme_res.theme
+            if theme in themes_total:
+                themes_total[theme][0] += theme_res.solved
+                themes_total[theme][1] += theme_res.total
+            else:
+                themes_total[theme] = [theme_res.solved, theme_res.total]
             cur_res.solved += theme_res.solved
             cur_res.total += theme_res.total
         cur_res.percent = solved_percent(cur_res)  
         result.append(cur_res)
+
+    total_res = PartResult("Всего")
+    for theme in themes_total:
+        solved, total = themes_total[theme]
+        total_res.themes.append([theme.name, solved, total, solved * 100 // total])
+        total_res.solved += solved
+        total_res.total += total
+    total_res.percent = solved_percent(total_res)
+    total_res.themes.sort(key=lambda theme: theme[3], reverse=True)
+    result.append(total_res)
+
     return result
