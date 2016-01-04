c47467e523f33e52f56b77158c6065b8bd30e1a9
Ivan Kriuchkov <johnhook059@gmail.com>
2016-01-04 18:40:11 +0300

    #3

--- web/doreshka/doreshka_by_user.py
+++ web/doreshka/doreshka_by_user.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from stats.models import User
+from stats.models import User, Language
 from .models import UserResult
+
+def lang_name_by_lang_id(id):
+    return Language.objects.filter(id=id).get().name
 
 def choose_form(a, form1, form2, form5):
     if a % 10 in {0, 5, 6, 7, 8, 9} or 11 <= a <= 19:
@@ -37,6 +40,15 @@ def perfect_by_user(user_id):
         return 0
 
     return ures.pf
+
+def lang_by_user(user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        ures = UserResult.objects.filter(user=user)[0]
+    except IndexError:
+        return 0
+
+    return lang_name_by_lang_id(ures.lg)
 
 def get_time_str(time):
     hr, mn = time // 3600, time // 60 % 60
